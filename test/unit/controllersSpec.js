2919c96fd173c9a9401f98ee3f5a0c32aed5cd2d
tuanhq <hoangquoctuan1985@gmail.com>
2014-12-18 14:11:32 +0700

    tutorial step 5

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,24 +3,21 @@
 /* jasmine specs for controllers go here */
 describe('PhoneCat controllers', function() {
 
-describe('PhoneListCtrl', function() {
-  var scope, ctr;
+describe('PhoneListCtrl', function() {  
+  var scope,ctrl,$httpBackend;
 
   beforeEach(module('phonecatApp'));
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+    $httpBackend=_$httpBackend_;
+    $httpBackend.expectGet('phones/phones.json').respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+    scope = $rootScope.$new();
+    ctrl = $controller('PhoneListCtrl',{$scope:scope});
 
-  beforeEach(inject(function() {
-    scope = {};
-    ctrl = $controller('PhoneListCtrl', {$scope:scope});
   }));
 
-  it('should creat "phones" model with 3 phones', function(){
-    expect(scope.phones.length).toBe(3);
-  });
-
-
-  it('should set set default value of orderProp model', function() {
-     expect(scope.orderProp).toBe('age'); 
-  }); 
+  it('should create "phones" model with 2 phone fetched from xhr', function() {
+      expect(scope.phones).tobeUndefined();
+  });  
 
 });
 });
